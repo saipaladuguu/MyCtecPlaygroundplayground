56323081eb7fe24b429c9fa08f802c2f6b5939be
spal7580 <psaineeraj@gmail.com>
2016-09-21 11:34:52 -0600

    New

---
+++ Contents.swift
@@ -0,0 +1,21 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+var thisIsANumber = 32423
+print(thisIsANumber)
+print(str + "words")
+print(str + "\(thisIsANumber)")
+print(thisIsANumber + 123134)
+print(" 🔫 😂")
+let maximumNumberOfLoginAttempts = 10
+var currentLoginAttempt = 0
+
+
+var x = 0.0, y = 0.0, z = 0.0
+
+var welcomeMessage: String
+
